68f34923595e4a558ce70227216fa04a6acaa1e3
manuelbasanta <manuelbasanta1@gmail.com>
2023-12-07 12:18:50 -0300

    fix

--- index.js
+++ index.js
@@ -77,6 +77,6 @@ app.delete('/api/notes/:id', (req, res) => {
 app.use(unknownEndpoint)
 
 const PORT = process.env.PORT || 3001
-app.listen(PORT, () => [
+app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
-])
+})
